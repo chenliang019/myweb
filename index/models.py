c832192c078f6711d37d05b46acca58046985ab4
test <chenliang02@bwoil.com>
2020-01-16 17:18:31 +0800

    增加测试——表单

--- index/models.py
+++ index/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=20)
+
+class Type(models.Model):
+    id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=20)
